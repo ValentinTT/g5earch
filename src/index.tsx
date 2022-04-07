157bbcdee2c62dbefd533c512d9eb4258e4cad03
ValentinTT <valentintapiatorti@gmail.com>
2022-04-07 02:21:23 -0300

    Fixing root height over 100vh
    The problem was a "," in the index.tsx after the <App/>

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ if (!rootElement) throw new Error('Failed to find the root element')
 const root = createRoot(rootElement)
 root.render(
   <React.StrictMode>
-    <App />,
+    <App />
   </React.StrictMode>
 )
 
