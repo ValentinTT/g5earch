016d741e88e8d424aabb734d49fe69694d31e99b
ValentinTT <valentintapiatorti@gmail.com>
2022-04-07 02:03:53 -0300

    Fix: text gradient  on firefox broken when translateX

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -9,16 +9,23 @@ export default function Title({ isFocus, children }: G5earchTitleProps) {
   const textColor =
     'text-transparent bg-clip-text bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500'
   return (
-    <h1
+    <div
       className={clsx(
-        'mb-3 z-10',
-        'font-semibold select-none',
-        'transition-all ease-in-out',
         textColor,
-        isFocus ? 'text-6xl' : 'text-3xl -translate-x-12 translate-y-12'
+        'transition-all ease-in-out',
+        isFocus ? '' : ' -translate-x-12 translate-y-12'
       )}
     >
-      {children}
-    </h1>
+      <h1
+        className={clsx(
+          'mb-3 z-10',
+          'font-semibold select-none',
+          'transition-all ease-in-out',
+          isFocus ? 'text-6xl' : 'text-3xl'
+        )}
+      >
+        {children}
+      </h1>
+    </div>
   )
 }
