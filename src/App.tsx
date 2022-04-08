369dd1d063cb073fe3aa2bbf6d31fd89b8024764
ValentinTT <valentintapiatorti@gmail.com>
2022-04-08 16:50:19 -0300

    connecting with api via proxy
    We need to find out how to connect to the api, but first we should know where we are going to deploy the api server or if we are gonna expose locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import clsx from 'clsx'
-import { createServer } from 'miragejs'
 import { ThemeButton } from 'components/ThemeButton'
 import Title from 'components/Title'
 import SearchBar from 'components/SearchBar'
@@ -8,36 +7,19 @@ import LoadingSpinner from 'components/LoadingSpinner'
 import { SearchResultResponse } from './@types/searchResultResponse'
 import SearchResult, { SearchResultTable } from 'components/SearchResult'
 
-let server = createServer({})
-server.get('/api/buscar/:text', (schema, req): SearchResultResponse[] => {
-  console.log(req)
-  return Array.from({ length: Math.floor(Math.random() * 7 + 1) }, () => ({
-    link: 'https://www.anderson1.org/site/handlers/filedownload.ashx?moduleinstanceid=24440&dataid=44258&FileName=hobbit.pdf',
-    title: 'The Hobbit',
-    preview:
-      'In a hole in the ground there lived a hobbit. Not a nasty, dirty, wet hole, filled with the ends of worms and an oozy smell, nor yet a dry, bare, sandy hole with nothing in it to sit down on or to eat: it was a hobbit-hole, and that means comfort. ',
-  }))
-})
-
 export default function App() {
   const [isLoading, setIsLoading] = useState(false)
   const [isFocus, setFocus] = useState(false)
-  const [searchResults, setSearchResults] = useState<SearchResultResponse[]>(
-    Array.from({ length: Math.floor(5) }, () => ({
-      link: 'https://www.anderson1.org/site/handlers/filedownload.ashx?moduleinstanceid=24440&dataid=44258&FileName=hobbit.pdf',
-      title: 'The Hobbit',
-      preview:
-        'In a hole in the ground there lived a hobbit. Not a nasty, dirty, wet hole, filled with the ends of worms and an oozy smell, nor yet a dry, bare, sandy hole with nothing in it to sit down on or to eat: it was a hobbit-hole, and that means comfort. ',
-    }))
-  )
+  const [searchResults, setSearchResults] = useState<SearchResultResponse[]>()
 
   const handleButtonClicked = (searchText: string) => {
     if (!isFocus) return setFocus(true)
+    if (searchText === '') return
     setIsLoading((_) => {
-      fetch('/api/buscar/' + encodeURIComponent(searchText))
+      fetch('/buscar?text=' + encodeURIComponent(searchText))
         .then((res) => res.json())
-        .then((json) => {
-          setSearchResults(json)
+        .then((json: { response: SearchResultResponse[] }) => {
+          setSearchResults(json.response)
           setIsLoading(false)
         })
       return true
