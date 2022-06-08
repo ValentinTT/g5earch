7b717dbdbed3d39d4771a4a41769f4bf39a5fe20
ValentinTT <valentintapiatorti@gmail.com>
2022-06-07 21:14:11 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,7 @@ import reportWebVitals from './reportWebVitals'
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Failed to find the root element')
 const root = createRoot(rootElement)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
